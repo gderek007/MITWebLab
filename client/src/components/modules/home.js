f90c476eb7c809e6d09eae34abd381de0d6dcab4
Samuel-CHLam <chun.lam18@imperial.ac.uk>
2021-01-16 01:44:07 +0000

    homepage todos!!

--- client/src/components/modules/home.js
+++ client/src/components/modules/home.js
@@ -1,7 +1,34 @@
 import React, {Component} from "react";
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+    };
+
     render() {
-        
-    }
+        return (
+            <>
+            <div>
+                {/*Todo: I need a container for all of these*/}
+                <div>
+                    {/*Todo: I need a class for title */}
+                    Create a party!
+                </div>
+                <div>
+                    {/*Todo: I need a class for subtitle */}
+                    Join! Have Fun!
+                </div>
+                <div>
+                    {/*Todo: I need a class for all the buttons */}
+                    <div>
+                        {/*Todo: Button linking to Login*/}
+                    </div>
+                    <div>
+                        {/*Todo: Button linking to event map*/}
+                    </div>
+                </div>
+            </div>
+            </>    
+        )
+    } 
 }

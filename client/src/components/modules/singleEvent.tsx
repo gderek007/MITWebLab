3cf1d8465c5995b6e019552b52f2320f5ce2cd48
Derek Garcia <derek@dereks-mbp.home>
2021-01-18 15:38:28 -0600

    Implemented Class for Single Event

--- client/src/components/modules/singleEvent.tsx
+++ client/src/components/modules/singleEvent.tsx
@@ -1,11 +1,52 @@
-import React, { Component } from "react";
-class SingleEvent extends Component {
-    constructor(props) {
-      super(props);
-    }
+// import React, { Component } from "react";
+// class SingleEvent extends Component {
+//     constructor(props) {
+//       super(props);
+//     }
 
-    // render() {
-    // }
+//     // render() {
+//     // }
+// }
+
+// export default SingleEvent
+
+export class SingleEvent {
+  eventName: string;
+  creatorId: string;
+  creatorName: string;
+  address: string;
+  description: string;
+  date: Date;
+  interested: number;
+  attending: number;
+
+  constructor(eventName: string, creatorId: string, creatorName: string, 
+              address:string, description:string, date: Date, 
+              interested:number, attending:number) {
+    this.eventName = eventName;
+    this.creatorId = creatorId;
+    this.creatorName = creatorName;
+    this.address = address;
+    this.description = description;
+    this.date = date;
+    this.interested = interested;
+    this.attending = attending;
+  }
+
+  incrementInterested() {
+    this.interested += 1;
+  }
+
+  decrementInterested() {
+    this.interested -= 1;
+  }
+
+  incrementAttending() {
+    this.attending += 1;
+  }
+
+  decrementAttending() {
+    this.attending -= 1;
+  }
 }
 
-export default SingleEvent

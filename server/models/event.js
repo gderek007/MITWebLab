00916ce4e957194b7b1c774f4b9e51d48cc17308
Derek Garcia <derek@dereks-mbp.home>
2021-01-18 20:24:13 -0600

    restored event schema

--- server/models/event.js
+++ server/models/event.js
@@ -1,32 +1,12 @@
 const mongoose = require("mongoose");
 
 const EventSchema = new mongoose.Schema({
-<<<<<<< HEAD
     host: String,
     host_id: String,
     nameEvent: String,
     date: Date,
     address: String,
     description: String,
-=======
-    // event_name: String,
-    nameEvent: String,
-    // creator_id: String,
-    // creator_name: String,
-    host: String,
-    host_id: String,
-    // start: String,
-    // end: String,
-    // timezone: String,
-    date: Date,
-    // venue: String,
-    address: String,
-    event_url: String,
-    // summary: String,
-    description: String,
-    Image_id: String,
-    rate: Number,
->>>>>>> 790e9403b2b915cc5b401c5faba41819984d1f2c
     interested: Number,
     attending: Number,
 });

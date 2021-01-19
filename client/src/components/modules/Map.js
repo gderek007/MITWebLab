013c8f5e79d267744518fd086d2292b4984b3423
mperaza <mperaza@mit.edu>
2021-01-19 01:24:44 -0800

    map is a class

--- client/src/components/modules/Map.js
+++ client/src/components/modules/Map.js
@@ -1,25 +1,35 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 import ReactMapGL from "react-map-gl";
 
 const API_TOKEN = "pk.eyJ1IjoibXBlcmF6YTA3MTQiLCJhIjoiY2trM2wxaXcyMTRwaTJ4cGpiaXQ3bjltNiJ9.a8AxmhpMBO7jfrD3s190Yg";
 
-export default function Map() {
-const [viewport, setViewport] = useState({
-  latitude: 42.36025746273741,
-  longitude: -71.09418145961277,
-  zoom: 14,
-  width: "100vw",
-  height: "100vh",
-});
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 42.35531356439811, 
+        longitude: -71.09148101699134,
+        zoom: 14,
+        width: "100vw",
+        height: "100vh",
+      },
+    };
+  }
 
-  return <div>
-    <ReactMapGL {...viewport} 
-    mapboxApiAccessToken={API_TOKEN}
-    mapStyle="mapbox://styles/mperaza0714/ckk3lrmmk0yyb17nzlv8s29er"
-    onViewportChange={viewport => {
-      setViewport(viewport);
-    }}>
-      
-    </ReactMapGL>
-  </div>
+  render() {
+    return ( 
+    <div>
+      <ReactMapGL {...this.state.viewport} 
+      mapboxApiAccessToken={API_TOKEN}
+      mapStyle="mapbox://styles/mperaza0714/ckk3lrmmk0yyb17nzlv8s29er"
+      onViewportChange={viewport => this.setState({viewport})}
+      >
+        
+      </ReactMapGL>
+    </div>
+    );
+  }
 }
+
+export default Map;

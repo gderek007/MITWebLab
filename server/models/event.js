0f5487845a8915cc6308dea2333736e1d4af7e3a
Samuel-CHLam <chun.lam18@imperial.ac.uk>
2021-01-19 01:10:03 +0000

    updated Schema

--- server/models/event.js
+++ server/models/event.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 
 const EventSchema = new mongoose.Schema({
-    event_name: String,
-    creator_id: String,
-    creator_name: String,
-    start: String,
-    end: String,
-    timezone: String,
-    venue: String,
+    // event_name: String,
+    nameEvent: String,
+    // creator_id: String,
+    // creator_name: String,
+    host: String,
+    host_id: String,
+    // start: String,
+    // end: String,
+    // timezone: String,
+    date: Date,
+    // venue: String,
+    address: String,
     event_url: String,
-    summary: String,
+    // summary: String,
+    description: String,
     Image_id: String,
     rate: Number,
+    interested: Number,
+    attending: Number,
 });
 
 // compile model from schema

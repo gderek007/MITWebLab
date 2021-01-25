28b33180f61165333216348f5d4ced369361d39e
mperaza <mperaza@mit.edu>
2021-01-25 02:30:26 -0800

    Update Map.js

--- client/src/components/modules/Map.js
+++ client/src/components/modules/Map.js
@@ -5,14 +5,16 @@ import { get } from "../../utilities";
 import "./Map.css";
 
 const API_TOKEN = "pk.eyJ1IjoibXBlcmF6YTA3MTQiLCJhIjoiY2trM2wxaXcyMTRwaTJ4cGpiaXQ3bjltNiJ9.a8AxmhpMBO7jfrD3s190Yg";
+const MIT_LAT = 42.35531356439811;
+const MIT_LNG = -71.09148101699134;
 
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewport: {
-        latitude: 42.35531356439811, 
-        longitude: -71.09148101699134,
+        latitude: MIT_LAT, 
+        longitude: MIT_LNG,
         zoom: 14,
         width: "99vw",
         height: "89vh",
@@ -50,7 +52,7 @@ class Map extends Component {
               className="marker-btn" 
               onClick={(e) => {
                 e.preventDefault();
-                const viewport = {
+                const markerViewport = {
                     ...this.state.viewport,
                     longitude: eventObj.lng,
                     latitude: eventObj.lat,
@@ -58,7 +60,7 @@ class Map extends Component {
                     transitionDuration: 1000,
                     transitionInterpolator: new FlyToInterpolator(),
                 };
-                this.setState({viewport});
+                this.setState({viewport: markerViewport});
                 this.setState({selectedEvent: eventObj});
               }}
             >
@@ -73,6 +75,15 @@ class Map extends Component {
             longitude={this.state.selectedEvent.lng}
             onClose={() => {
               this.setState({selectedEvent: null});
+              const resetViewport = {
+                ...this.state.viewport,
+                longitude: MIT_LNG,
+                latitude: MIT_LAT,
+                zoom: 14,
+                transitionDuration: 1000,
+                transitionInterpolator: new FlyToInterpolator(),
+            };
+            this.setState({viewport: resetViewport});
             }}
           >
             <div>

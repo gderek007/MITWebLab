38e65d8f64bf2ca8a53680139059aed25044c22f
Samuel-CHLam <chun.lam18@imperial.ac.uk>
2021-01-26 22:00:30 +0000

    Added fields for editing profile

--- client/src/components/modules/EditProfileLeft.js
+++ client/src/components/modules/EditProfileLeft.js
@@ -16,6 +16,10 @@ import "./EditProfileLeft.css";
          super(props);
          this.state = {
              name: "",
+             user_nickname: "",
+             based: "",
+             email: "",
+             facebook_name: "",
          }
      }
 
@@ -26,6 +30,29 @@ import "./EditProfileLeft.css";
         });
       };
 
+      handleChangeNickName = (nickName) => {
+        this.setState({
+          user_nickname: nickName.target.value,
+        });
+      };
+
+      handleChangeBased = (based) => {
+        this.setState({
+          based: based.target.value,
+        });
+      };
+
+      handleChangeEmail = (email) => {
+        this.setState({
+          email: email.target.value,
+        });
+      };
+
+      handleChangeFacebook = (facebook) => {
+        this.setState({
+          facebook_name: facebook.target.value,
+        });
+      };
      // Updating User Details
 
      // TODO: Rendering
@@ -37,6 +64,30 @@ import "./EditProfileLeft.css";
                     onChange={this.handleChangeName.bind(this)} 
                     value={this.state.name} 
                     placeholder = {this.props.user.name}/>
+                </label>
+                <label>
+                    <input type="nickname" 
+                    onChange={this.handleChangeNickName.bind(this)} 
+                    value={this.state.user_nickname} 
+                    placeholder = {this.props.user.user_nickname}/>
+                </label>
+                <label>
+                    <input type="based" 
+                    onChange={this.handleChangeBased.bind(this)} 
+                    value={this.state.based} 
+                    placeholder = {this.props.user.based}/>
+                </label>
+                <label>
+                    <input type="email" 
+                    onChange={this.handleChangeEmail.bind(this)} 
+                    value={this.state.email} 
+                    placeholder = {this.props.user.email}/>
+                </label>
+                <label>
+                    <input type="facebook" 
+                    onChange={this.handleChangeFacebook.bind(this)} 
+                    value={this.state.facebook_name} 
+                    placeholder = {this.props.user.facebook_name}/>
                 </label>
             </form>
          )
